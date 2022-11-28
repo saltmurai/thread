819ed13c1642f5d2fbb023fcb907e1465f9f6c00
saltmurai <phamsang6260@gmail.com>
2022-11-28 11:02:49 +0700

    feat: init

---
+++ README.md
@@ -0,0 +1,14 @@
+# How to run
+Install psutil
+
+```
+pip install -r requirements.txt
+```
+
+
+Run
+
+```
+python3 thread.py
+```
+
---
+++ thread.py
@@ -0,0 +1,38 @@
+import threading
+import time
+import psutil 
+
+def print_time( threadName, delay):
+   count = 0
+   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+
+
+def thread_time(target, threadName):
+   def wrapper(*args, **kwargs):
+       st = time.time()
+       try:
+           return target(*args, **kwargs)
+       finally:
+           et = time.time()
+           print(f"Execution time of {threadName}: {et - st}")
+           print(f"Finish {threadName} RAM useage {psutil.virtual_memory().percent}")
+           threading.currentThread().duration = et - st
+   return wrapper
+
+if __name__ =="__main__":
+	# creating thread
+	t1 = threading.Thread(target=thread_time(print_time, "thread 1"), args=("Thread 1", 2))
+	t2 = threading.Thread(target=thread_time(print_time, "thread 2"), args=("Thread 2", 1))	
+	# starting thread 1
+	t1.start()
+	# starting thread 2
+	t2.start()
+	# wait until thread 1 is completely executed
+	t1.join()
+	# wait until thread 2 is completely executed
+	t2.join()
+	# both threads completely executed
+	print("Done!")
