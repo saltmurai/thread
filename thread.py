aaad9965c4b26bf24a9f5f0db15762ec33cbf4db
saltmurai <phamsang6260@gmail.com>
2022-12-05 13:55:44 +0700

    fix

--- thread.py
+++ thread.py
@@ -1,40 +1,43 @@
 import threading
 import time
-import psutil 
+import random as rand
+import psutil
 
-def print_time( threadName, delay):
-   count = 0
-   while count < 5:
+# Create a lock object to use as a mutex
+lock = threading.Lock()
+
+# Define a function that will be run by each thread
+def thread_function(name, delay):
+
+  # Acquire the lock to access the shared resource
+  start_time = time.perf_counter()
+  lock.acquire()
+  try:
+
+    # Access the shared resource here
+    print(f'Thread {name} is accessing the shared resource')
+    count = 0
+    while count < 5:
       time.sleep(delay)
       count += 1
-      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+      print(f'Thread {name}: {time.ctime(time.time())}')
+    # Release the lock when you are done
+  finally:
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    # Log the total execution time
+    print(f'Thread {name} executed in {execution_time:.2f} seconds')
+    thread_process = psutil.Process()
+    ram_used = thread_process.memory_info().rss
+    print(f'Thread {name} used {ram_used} bytes of RAM')
+    lock.release()
 
+# Create some threads and start them
+threads = []
+for i in range(5):
+  threads.append(threading.Thread(target=thread_function, args=(i, 2)))
+  threads[-1].start()
 
-def thread_time(target, threadName):
-   def wrapper(*args, **kwargs):
-       st = time.time()
-       try:
-           return target(*args, **kwargs)
-       finally:
-           et = time.time()
-           print(f"Execution time of {threadName}: {et - st}")
-           print(f"Finish {threadName} RAM useage {psutil.virtual_memory().percent}")
-           threading.currentThread().duration = et - st
-           print(f"Start time of {threadName}: {st}")
-           print(f"End time of {threadName}: {et}")
-   return wrapper
-
-if __name__ =="__main__":
-	# creating thread
-	t1 = threading.Thread(target=thread_time(print_time, "thread 1"), args=("Thread 1", 2))
-	t2 = threading.Thread(target=thread_time(print_time, "thread 2"), args=("Thread 2", 1))	
-	# starting thread 1
-	t1.start()
-	# starting thread 2
-	t2.start()
-	# wait until thread 1 is completely executed
-	t1.join()
-	# wait until thread 2 is completely executed
-	t2.join()
-	# both threads completely executed
-	print("Done!")
+# Wait for all threads to complete
+for thread in threads:
+  thread.join()
